73c8045b6dab3cd87adec044ed5cc50149f0c7b6
Thiago <thiago.bafi@outlook.com>
2024-05-29 01:09:21 -0300

    Update Board.cs

--- ChessLogic/Board.cs
+++ ChessLogic/Board.cs
@@ -27,5 +27,44 @@ namespace ChessLogic
             board.AddStartPieces();
             return board;
         }
+
+
+        private void AddStartPieces()
+        {
+            this[0,0] = new Rook(Player.Black);
+            this[0,1] = new Knight(Player.Black);
+            this[0,2] = new Bishop(Player.Black);  
+            this[0,3] = new Queen(Player.Black);
+            this[0,4] = new King(Player.Black);
+            this[0,5] = new Bishop(Player.Black);
+            this[0,6] = new Knight(Player.Black);
+            this[0,7] = new Rook(Player.Black);
+            this[1,0] = new Pawn(Player.Black);
+            this[1,1] = new Pawn(Player.Black);
+            this[1,2] = new Pawn(Player.Black); 
+            this[1,3] = new Pawn(Player.Black); 
+            this[1,4] = new Pawn(Player.Black);
+            this[1,5] = new Pawn(Player.Black);
+            this[1,6] = new Pawn(Player.Black);
+            this[1,7] = new Pawn(Player.Black);
+
+            this[7,0] = new Rook(Player.White);
+            this[7,1] = new Knight(Player.White);
+            this[7,2] = new Bishop(Player.White);
+            this[7,3] = new Queen(Player.White);
+            this[7,4] = new King(Player.White);
+            this[7,5] = new Bishop(Player.White);
+            this[7,6] = new Knight(Player.White);
+            this[7,7] = new Rook(Player.White);
+            this[6,0] = new Pawn(Player.White);
+            this[6,1] = new Pawn(Player.White); 
+            this[6,2] = new Pawn(Player.White); 
+            this[6,3] = new Pawn(Player.White);
+            this[6,4] = new Pawn(Player.White);
+            this[6,5] = new Pawn(Player.White);
+            this[6,6] = new Pawn(Player.White);
+            this[6,7] = new Pawn(Player.White);
+
+        }
     }
 }
