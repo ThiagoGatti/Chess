be4aa7db2773a93d927eeda963d5584820a3ee95
Thiago <thiago.bafi@outlook.com>
2024-05-29 01:02:57 -0300

    Create Board.cs

---
+++ ChessLogic/Board.cs
@@ -0,0 +1,31 @@
+﻿
+
+namespace ChessLogic
+{
+    public class Board
+    {
+        private readonly Piece[,] pieces = new Piece[8, 8];
+        
+
+        public Piece this[int row, int col]
+        {
+            get { return pieces[row, col]; }
+            set { pieces[row, col] = value; }
+        }
+
+        public Piece this[Position pos]
+        {
+
+        get { return pieces[pos.Row, pos.Column]; }
+        set { this[pos.Row, pos.Column] = value; }
+    }
+    
+
+       public static Board Initial()
+        {
+            Board board = new Board();
+            board.AddStartPieces();
+            return board;
+        }
+    }
+}
